5f63d9da43355b574b75c07f9aabaee759f0e26e
TYehan <bandaratharindu17@gmail.com>
2025-02-17 14:56:19 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+Run the batch file before starting your work:
+
+```bash
+set_env.bat
+```
+
+Then the programs can be run.
---
+++ set_enc.bat
@@ -0,0 +1,5 @@
+@REM "E:\Programs\NuSMV-2.7.0-win64\bin" is the path to the NuSMV executables
+
+set PATH=%PATH%;E:\Programs\NuSMV-2.7.0-win64\bin
+set INCLUDE_PATH=E:\Programs\NuSMV-2.7.0-win64\include
+set LIBRARY_PATH=E:\Programs\NuSMV-2.7.0-win64\lib
